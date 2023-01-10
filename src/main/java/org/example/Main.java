080a236f9c1db433f40a735b4b878b0b231817fc
Ayoub Abedalhameed <ayoubabdalhameed@outlook.com>
2023-01-10 16:54:57 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import io.pkts.packet.IPv4Packet;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    IPv4Packet
+}
---
+++ src/main/java/org/example/handlers/RequestHandler.java
@@ -0,0 +1,8 @@
+package org.example.handlers;
+
+import io.pkts.PacketHandler;
+
+public class RequestHandler implements PacketHandler{
+    io.pkts.protocol.Protocol
+
+}
